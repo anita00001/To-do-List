6e99d06c84577cfaab82a179b5c11ee4550d7b6b
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-03-09 13:35:19 +0100

    Create edit.test.js file

---
+++ src/edit.test.js
@@ -0,0 +1,19 @@
+const editTask = require('./edit.js');
+
+const previousTask = [
+  { description: 'Task1', completed: false, index: 1 },
+  { description: 'Task2', completed: false, index: 2 },
+  { description: 'Task3', completed: false, index: 3 },
+];
+
+const newTask = [
+  { description: 'Task1', completed: false, index: 1 },
+  { description: 'Update GitHub Profile', completed: false, index: 2 },
+  { description: 'Task3', completed: false, index: 3 },
+];
+
+test('Update ToDo item', () => {
+  expect(
+    editTask(previousTask, 2 - 1, 'Update GitHub Profile'),
+  ).toEqual(newTask);
+});
